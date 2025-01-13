627472d951d2f3aa3ae53d3006e133e8ef196253
media-frameworks <mikehallvideo@gmail.com>
2025-01-12 22:02:26 -0500

    path separator

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
+
 const server = require('./admin/server.json')
+const SEPARATOR = path.sep;
 
 app = express();
 const PORT = server.port
@@ -10,20 +13,20 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 app.use(cors({
-   origin: "*"
+    origin: "*"
 }));
 
 app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
 
-const FRAMES_DIR = `.\\frames`
+const FRAMES_DIR = `.${SEPARATOR}frames`
 if (!fs.existsSync(FRAMES_DIR)) {
-   console.log("adding frames dir", FRAMES_DIR)
-   fs.mkdirSync(FRAMES_DIR);
+    console.log("adding frames dir", FRAMES_DIR)
+    fs.mkdirSync(FRAMES_DIR);
 }
 
 app.post("/new_video", (req, res) => {
-   const video_id = req.query.video_id
-   const video_dir  = `${FRAMES_DIR}\\${video_id}`
-   fs.mkdirSync(video_dir);
-   return res.json({result: `created dir ${video_dir}`})
+    const video_id = req.query.video_id
+    const video_dir = `${FRAMES_DIR}${SEPARATOR}${video_id}`
+    fs.mkdirSync(video_dir);
+    return res.json({video_dir: video_dir})
 });
